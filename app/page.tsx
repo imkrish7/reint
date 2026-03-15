d399d916364705559fe6b740ba66dc78b0279a22
Rackham <singhkrishk07@gmail.com>
2026-03-16 01:18:02 +0530

    small text correction

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <p className="text-xl text-gray-500">
-          To see the stats, Please link below
+          To see the stats, Please click the link below
         </p>
         <Link href="/dashboard">
           <Button>Dashboard</Button>
